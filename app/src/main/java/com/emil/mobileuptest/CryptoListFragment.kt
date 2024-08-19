d66059ff004e9b70c8dd6a2436800c56848f13f6
emilbagirov30 <emil_bgr@mail.ru>
2024-08-19 22:47:57 +0300

    added a fragment container and transferred the data

---
+++ app/src/main/java/com/emil/mobileuptest/CryptoListFragment.kt
@@ -0,0 +1,83 @@
+package com.emil.mobileuptest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+
+
+class CryptoListFragment : Fragment() {
+    private lateinit var viewModel: CryptoViewModel
+    private lateinit var cryptoAdapter: CryptoAdapter
+    private lateinit var rvCryptoList: RecyclerView
+    private lateinit var chipGroup: ChipGroup
+    private lateinit var chipUsd: Chip
+    private lateinit var chipRub: Chip
+    private var currentCurrency = "usd"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_crypto_list, container, false)
+        viewModel = ViewModelProvider(this).get(CryptoViewModel::class.java)
+        rvCryptoList = view.findViewById(R.id.rv_crypto_list)
+        chipGroup = view.findViewById(R.id.chip_group)
+        chipUsd =view.findViewById(R.id.chip_usd)
+        chipRub =view.findViewById(R.id.chip_rub)
+        cryptoAdapter = CryptoAdapter(emptyList(), currentCurrency)
+        rvCryptoList.layoutManager = LinearLayoutManager(requireContext())
+        rvCryptoList.adapter = cryptoAdapter
+        viewModel = ViewModelProvider(this).get(CryptoViewModel::class.java)
+
+        cryptoAdapter = CryptoAdapter(emptyList(), currentCurrency)
+        rvCryptoList.layoutManager = LinearLayoutManager(requireActivity())
+        rvCryptoList.adapter = cryptoAdapter
+
+
+        viewModel.cryptoList.observe(requireActivity()) { cryptos ->
+            cryptoAdapter.updateData(cryptos, currentCurrency)
+            MainActivity.loading.visibility = View.GONE
+        }
+
+        viewModel.error.observe(requireActivity()) { errorMessage ->
+            Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+            MainActivity.loading.visibility = View.GONE
+        }
+
+
+        fetchCryptos(currentCurrency)
+        chipUsd.isChecked = true
+        chipUsd.setOnClickListener {
+            currentCurrency = "usd"
+            fetchCryptos(currentCurrency)
+        }
+
+        chipRub.setOnClickListener {
+            currentCurrency = "rub"
+            fetchCryptos(currentCurrency)
+        }
+        return  view
+    }
+
+    private fun fetchCryptos(currency: String) {
+        MainActivity.loading.visibility = View.VISIBLE
+        viewModel.fetchCryptos(currency)
+    }
+
+    }
+
+
--- app/src/main/java/com/emil/mobileuptest/MainActivity.kt
+++ app/src/main/java/com/emil/mobileuptest/MainActivity.kt
@@ -13,60 +13,21 @@ import com.google.android.material.chip.ChipGroup
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: CryptoViewModel
-    private lateinit var cryptoAdapter: CryptoAdapter
+companion object {
+  lateinit var loading: ProgressBar
 
-    private lateinit var rvCryptoList: RecyclerView
-    private lateinit var pbLoading: ProgressBar
-    private lateinit var chipGroup: ChipGroup
-    private lateinit var chipUsd: Chip
-    private lateinit var chipRub: Chip
-
-    private var currentCurrency = "usd"
+}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        rvCryptoList = findViewById(R.id.rv_crypto_list)
-        pbLoading = findViewById(R.id.pb_loading)
-        chipGroup = findViewById(R.id.chip_group)
-        chipUsd = findViewById(R.id.chip_usd)
-        chipRub = findViewById(R.id.chip_rub)
+       loading = findViewById(R.id.pb_loading)
+        val cryptoListFragment = CryptoListFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, cryptoListFragment)
+            commit()
 
 
-        viewModel = ViewModelProvider(this).get(CryptoViewModel::class.java)
-
-        cryptoAdapter = CryptoAdapter(emptyList(), currentCurrency)
-        rvCryptoList.layoutManager = LinearLayoutManager(this)
-        rvCryptoList.adapter = cryptoAdapter
-
-
-        viewModel.cryptoList.observe(this) { cryptos ->
-            cryptoAdapter.updateData(cryptos, currentCurrency)
-            pbLoading.visibility = View.GONE
         }
-
-        viewModel.error.observe(this) { errorMessage ->
-            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
-            pbLoading.visibility = View.GONE
-        }
-
-
-        fetchCryptos(currentCurrency)
-        chipUsd.isChecked = true
-        chipUsd.setOnClickListener {
-            currentCurrency = "usd"
-            fetchCryptos(currentCurrency)
-        }
-
-        chipRub.setOnClickListener {
-            currentCurrency = "rub"
-            fetchCryptos(currentCurrency)
-        }
-    }
-
-    private fun fetchCryptos(currency: String) {
-        pbLoading.visibility = View.VISIBLE
-        viewModel.fetchCryptos(currency)
     }
 }
